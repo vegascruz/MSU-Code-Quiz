c4843d97f0f9d3d28ab4988bdf363596fc2199cb
Vegascruz <cruzv@students.westerntc.edu>
2022-09-11 20:40:50 -0500

    Made the basic structure and functionality of the API. Need to be able to append answers for each question.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,59 @@
-// Selects element by class
+// Selects the span tag that is for our timer
 var timer = document.querySelector(".timer");
+var secondsLeft = 91;
 
+const startButton = document.getElementById("btnStart");
+const questionContainerElement = document.getElementById('questionContainer');
+const mainMenu = document.getElementById('mainMenu');
+const finalPage = document.getElementById('finalPage');
+const questionElement = document.getElementById('question');
+const answerButtonElement = document.getElementById('answerButtons');
 
-var secondsLeft = 90;
+let shuffledQuestions, currentQuestionIndex;
 
+//             FUNCTIONS             //
+//this function will display the questions and answers based onClick
+function startQuiz(){
+  //hides start menu
+  mainMenu.classList.add('hide');
+  startButton.classList.add('hide');
+  //Shuffles questions
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  //shows first questions
+  questionContainerElement.classList.remove('hide');
+  answerButtonElement.classList.remove('hide');
+  setNextQuestion();
+  setTime();
+}
+
+//this will set the question based on an array of shuffled questions
+function setNextQuestion(){
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+//this will display the questions/answers on each slide
+function showQuestion(question){
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+  const button = document.createElement('button');
+  button.innerText = answer.text;
+  button.classList.add('answerBtn')
+  if(answer.correct){
+    //add code here
+  }
+  button.addEventListener('click', nextSet)
+  });
+}
+
+//this function will display the start menu based onClick
+function nextSet(){
+  questionContainerElement.classList.add('hide');
+  answerButtonElement.classList.add('hide');
+  finalPage.classList.remove('hide');
+}
+
+//function for the timer
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
@@ -18,4 +68,35 @@ function setTime() {
 
   }, 1000);
 }
-setTime();
+//setTime();  WE WILL USE THIS ONCE THE USER CLICKS 'START QUIZ'
+
+//this is my array of questions and answers: only 3 based on the mockup
+const questions = [
+  {
+    question: "Arrays in JavaScript can be used to store ________. ",
+    answers:[
+      {text: '1. numbers and strings', correct: false},
+      {text: '2. other arrays', correct: false },
+      {text: '3. booleans', correct: false},
+      {text: '4. all of the above', correct: true}
+    ]
+  },
+  {
+    question: "String values must be enclosed within ________ when beind assigned to variables.",
+    answers:[
+      {text: '1. commas', correct: false},
+      {text: '2. curly brackets', correct: false},
+      {text: '3. quotes', correct: true},
+      {text: '4. parenthisis', correct: false}
+    ]
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers:[
+      {text: '1. JavaScript', correct: false},
+      {text: '2. terminal/bash', correct: false},
+      {text: '3. for loops', correct: false},
+      {text: '4. console.log', correct: true}
+    ]
+  }
+]
